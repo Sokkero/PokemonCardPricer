52d41f8009f6e7b2b0500ca17b969c2bef053ebc
Paul Sohns <paul.sohns@web.de>
2025-06-27 17:44:01 +0200

    Some stuff

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
+def _createDriver():
+    option = webdriver.ChromeOptions()
+
+    # Fake a non-headless browser, making sites with weak bot protection think you're a regular browser
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+    option.add_argument(f'user-agent={user_agent}')
+
+    # Remove everything that the browser doesn't need to improve performance
+    option.add_argument('--headless')
+    option.add_argument("--disable-infobars")
+    option.add_argument("--disable-extensions")
+    option.add_argument("--disable-blink-features=AutomationControlled")
+
+    print("Starting selenium...")
+    try:
+        driver = webdriver.Chrome(options=option)
+    except:
+        print("Failed to start selenium!")
+        exit(1)
+
+    # Go to start page
+    driver.get("https://www.cardmarket.com/en/Pokemon")
+
+    print("Success")
+
+    return driver
+
 if __name__ == '__main__':
     # Cardmarket uses ids for languages
     languages = {
@@ -19,29 +46,7 @@ if __name__ == '__main__':
         "t-ch": 11
     }
 
-    option = webdriver.ChromeOptions()
-
-    # Fake a non-headless browser, making sites with weak bot protection think you're a regular browser
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    option.add_argument(f'user-agent={user_agent}')
-
-    # Remove everything that the browser doesn't need to improve performance
-    option.add_argument('--headless')
-    option.add_argument("--disable-infobars")
-    option.add_argument("--disable-extensions")
-    option.add_argument("--no-sandbox")
-    option.add_argument("--disable-dev-shm-usage")
-    option.add_argument("--incognito")
-
-    print("Starting selenium...")
-    try:
-        driver = webdriver.Chrome(options=option)
-    except Exception as e:
-        print("Failed to start selenium!")
-        print(e)
-        exit(1)
-
-    print("Success")
+    driver = _createDriver()
 
     while True:
         print()
@@ -59,30 +64,28 @@ if __name__ == '__main__':
             print("Invalid language")
             continue
 
-        # Go to page
-        driver.get("https://www.cardmarket.com/en/Pokemon")
-
         # Find the search input and enter card id
         try:
-            searchInput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//input[@id='ProductSearchInput']")))
+            searchInput = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f"//input[@id='ProductSearchInput']")))
+            oldUrl = driver.current_url
+            searchInput.send_keys(cardId)
+            searchInput.send_keys(Keys.ENTER)
+            searchInput = WebDriverWait(driver, 5).until_not(EC.url_to_be(oldUrl))
         except:
-            driver.save_screenshot("screenshot.png")
-            print("Page couldn't load (see saved screenshot for more details)")
+            print("Page couldn't load, most likely blocked due to too many requests, restart the program and try again in a few seconds")
             driver.close()
-            exit(1)
-
-        searchInput.send_keys(cardId)
-        searchInput.send_keys(Keys.ENTER)
+            exit(0)
 
         # Append language id to URL
         driver.get(driver.current_url + "?language=" + str(languageId))
+        print("URL: " + driver.current_url)
 
-        # Grep the first entry in the table (default sorted by cheapest)
+        # Grab the first entry in the table (default sorted by cheapest)
         try:
-            rows = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f"//div[contains(@id, 'articleRow')]")))
-            price = rows[0].find_element(By.XPATH, f"//div[@class='col-offer col-auto']//span[last()]").get_attribute('innerHTML')
+            firstRow = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f"//div[contains(@id, 'articleRow')][1]")))
+            price = firstRow.find_element(By.XPATH, f"//div[@class='col-offer col-auto']//span[last()]").get_attribute('innerHTML')
             print(price)
         except:
-            driver.save_screenshot("screenshot.png")
-            print("No offers found! (see saved screenshot for more details)")
-            continue
+            print("No offers found, most likely blocked due to too many requests, restart the program and try again in a few seconds")
+            driver.close()
+            exit(0)
